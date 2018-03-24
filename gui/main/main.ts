2a4ab0f80dc0c6841c42f8441ae00ffef3e44cf2
vincent <muehler.v@gmail.com>
2018-03-24 13:31:51 +0100

    add react and redux devtools

--- gui/main/main.ts
+++ gui/main/main.ts
@@ -13,16 +13,24 @@ function createWindow() {
   const protocol = 'http:' // 'file:'
   const pathname = 'localhost:3000' // path.join(__dirname, './index.html')
   mainWindow.loadURL(url.format({
-      pathname,
-      protocol,
-      slashes: true,
+    pathname,
+    protocol,
+    slashes: true,
   }))
 
   mainWindow.webContents.openDevTools()
   mainWindow.setResizable(true)
 }
 
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  process.env.REACT_DEVTOOLS_PATH
+    ? BrowserWindow.addDevToolsExtension(process.env.REACT_DEVTOOLS_PATH as string)
+    : console.log('failed to load react dev tools process.env.REACT_DEVTOOLS_PATH:', process.env.REACT_DEVTOOLS_PATH)
+  process.env.REDUX_DEVTOOLS_PATH
+    ? BrowserWindow.addDevToolsExtension(process.env.REDUX_DEVTOOLS_PATH as string)
+    : console.log('failed to load redux dev tools process.env.REDUX_DEVTOOLS_PATH:', process.env.REDUX_DEVTOOLS_PATH)
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
--- gui/renderer/app/redux/store.ts
+++ gui/renderer/app/redux/store.ts
@@ -1,10 +1,19 @@
-import { createStore, applyMiddleware } from 'redux'
+import { remote } from 'electron'
+import { createStore, applyMiddleware, compose  } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer'
 
 const store = process.env.NODE_ENV === 'development'
-  ? createStore(rootReducer, applyMiddleware(createLogger(), thunk))
+  ? createStore(rootReducer,
+      composeWithDevTools({})(
+        applyMiddleware(
+          createLogger(),
+          thunk
+        )
+      )
+    )
   : createStore(rootReducer, applyMiddleware(thunk))
 
 export default store
